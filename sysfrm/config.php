7aee040da86d550373b04cf818c57676561d7115
Nain Acero <a_nacerom@unjbg.edu.pe>
2020-07-14 10:03:48 -0500

    version3.1

--- sysfrm/config.php
+++ sysfrm/config.php
@@ -3,5 +3,5 @@ $db_host	    = 'bu79pllsgmrflzb3i27y-mysql.services.clever-cloud.com';
 $db_user        = 'uknyneu7jfbl5wso';
 $db_password	= 'z9k7DWa7gaP2sDRiNoUC';
 $db_name	    = 'bu79pllsgmrflzb3i27y';
-define('APP_URL', 'http://localhost/arduino101');
+define('APP_URL', 'https://arduino101.herokuapp.com/');
 $_app_stage = 'Dev'; // You can set this variable Live to Dev to enable ibilling Debug
